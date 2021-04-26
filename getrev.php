df6e0a31c3aec6500d0242f4bf0c1af929f6504f
Руслан Шаяхметов <58970010+lenor421@users.noreply.github.com>
2021-04-26 15:50:28 +0500

    Лаба №3 Задание 1,2

---
+++ getrev.php
@@ -0,0 +1,17 @@
+﻿<?php
+
+        $jsonString = file_get_contents("reviews.json", JSON_UNESCAPED_UNICODE);
+        $rrow = json_decode($jsonString);
+        $schiotchik=0;
+        $count = count($rrow->review);
+        foreach ($rrow->review as $number => $item) {
+            $schiotchik++;
+            $name = $item->name;
+            $email = $item->email;
+            $rev = $item->rev;
+            echo "<div style='width: 600px; border: 3px solid #000; padding:10px' >";
+            echo '<span style="font-weight: bold;">Имя: </span>'.$name.'<br><span style="font-weight: bold;">email: </span>'.$email.'<br><span style="font-weight: bold;">Отзыв: </span>'.$rev.'<br>';  
+            echo "</div><br>";
+            if($schiotchik==$count)break;
+        }
+?>
---
+++ test.php
@@ -0,0 +1,16 @@
+﻿<?php
+$tek=$_GET["tek"];
+$jsonString = file_get_contents("test.json");
+$rrow = json_decode($jsonString);
+$schiotchik=0;
+foreach ($rrow->news->new as $number => $item) {
+    $schiotchik++;
+    $zag = $item->zag;
+    $text = $item->text;
+    $img = $item->img;
+    echo "<div style='width: 600px; border: 3px solid #000; padding:10px' >";
+    echo 'Новость №'.$number.'<br><p style="font-weight: bold; font-size: 30px">'.$zag.'</p>'.$text.'<br><img src="'.$img.'" width=400px><br>';  
+    echo "</div><br>";
+    if($schiotchik==$tek)break;
+}
+?>
